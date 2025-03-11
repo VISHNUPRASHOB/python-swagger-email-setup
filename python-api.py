9150734e312f2d832ac02cad00355552e9ddc36f
VISHNUPRASHOB S <44727662+VISHNUPRASHOB@users.noreply.github.com>
2025-03-12 05:17:09 +0530

    Update python-api.py run code test 2

--- python-api.py
+++ python-api.py
@@ -254,9 +254,9 @@ def send_otp():
         return jsonify({'error': 'Failed to send OTP'}), 400  # Return an error response
     
 if __name__ == "__main__":
-
-    port = int(os.environ.get("PORT", 8080))  # Use dynamic port from Azure or fallback to 8080
+    port = int(os.environ.get("PORT", 8080))  # Use dynamic port from Azure
     app.run(host="0.0.0.0", port=port, debug=True, use_reloader=False, threaded=True)
+
 
 
 

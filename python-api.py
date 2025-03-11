70fce060be8e69ef9747ff8683af74df6e746872
VISHNUPRASHOB S <44727662+VISHNUPRASHOB@users.noreply.github.com>
2025-03-12 03:06:27 +0530

    Update python-api.py port 8080

--- python-api.py
+++ python-api.py
@@ -254,4 +254,4 @@ def send_otp():
         return jsonify({'error': 'Failed to send OTP'}), 400  # Return an error response
     
 if __name__ == '__main__':
-    serve(app, host="127.0.0.1", port=8081)
+    serve(app, host="127.0.0.1", port=8080)
